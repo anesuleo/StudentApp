fe286dea5a3721df4278aa3148844d7f570add22
Anesu Njove <G00425238@atu.ie>
2023-10-25 13:50:02 +0100

    Added Getters and Setters

--- src/main/java/ie/atu/Student.java
+++ src/main/java/ie/atu/Student.java
@@ -1,7 +1,62 @@
 package ie.atu;
 
+import java.util.Scanner;
+
 public class Student
 {
-    
+    String firstName;
+    String lastName;
+    String studentEmail;
+    String studentCourse;
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getStudentEmail() {
+        return studentEmail;
+    }
+
+    public void setStudentEmail(String studentEmail) {
+        this.studentEmail = studentEmail;
+    }
+
+    public String getStudentCourse() {
+        return studentCourse;
+    }
+
+    public void setStudentCourse(String studentCourse) {
+        this.studentCourse = studentCourse;
+    }
+
+    public void getUserInput()
+    {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter first name: ");
+        this.firstName = scanner.nextLine();
+
+        System.out.print("Enter last: ");
+        this.lastName = scanner.nextLine();
+
+        System.out.print("Enter your email: ");
+        this.studentEmail = scanner.nextLine();
+
+        System.out.print("Enter your email: ");
+        this.studentCourse = scanner.nextLine();
+
+    }
 
 }
